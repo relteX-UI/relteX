3c2190ae60e6ae9a9c9b7aef84c7086aeaa47470
Jeccoman <jeccopatrickoutlook.com>
2025-11-22 00:12:06 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
+};
+
+module.exports = nextConfig;
