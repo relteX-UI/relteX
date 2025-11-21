e16edd1191c455582400af2954c45cd39398dc7e
Jeccoman <jeccopatrickoutlook.com>
2025-11-22 00:12:14 +0300

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@nativeui/ui'],
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+};
+
+export default nextConfig;
